0b244b5c4ca9cc9798ece0773f80f37af777893e
Stefan Oljacic <djedpool@gmail.com>
2019-11-14 14:42:08 +0100

    Adding crucial settings for build start.

--- realestate-acf-plugin.php
+++ realestate-acf-plugin.php
@@ -1,10 +1,35 @@
 <?php
-
 /**
  * @package RealEstateACFPlugin
  */
 
 /*
- Plugin Name: RealEstate ACF Plugin
- Plugin URI:
+Plugin Name: RealEstate ACF Plugin
+Plugin URI: https://github.com/Oljacic/realestate-acf-plugin
+Description: This plugin is for testing my skills and ability to nail a job!
+Version: 1.0.0
+Author: Stefan "Stef" Oljacic
+License: GPLv2 or latter
+Text Domain: realestate-acf-plugin
  */
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 Automattic, Inc.
+*/
+
+defined('ABSPATH') or die('Hi there! What are you doing here, you can\' access this file, you silly human?');
+
