254f4119db5466db7ac4f528c0ae02e20de8cea2
Oljacic <djedpool@gmail.com>
2019-11-19 10:50:47 +0100

    Grinding up starter structure

--- inc/Init.php
+++ inc/Init.php
@@ -14,7 +14,8 @@ final class Init
    public static function get_services() {
         return [
             Pages\Admin::class,
-            Base\Enqueue::class
+            Base\Enqueue::class,
+            Base\SettingsLinks::class
         ];
    } 
 
