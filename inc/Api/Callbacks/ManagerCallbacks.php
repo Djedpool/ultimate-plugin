5d6da4ddbddc8736f2514a7c5f3fd6a2ca6b7932
Stefan Oljacic <djedpool@gmail.com>
2019-12-02 07:38:04 +0100

    Writing solution for bug on activate plugin.

--- inc/Api/Callbacks/ManagerCallbacks.php
+++ inc/Api/Callbacks/ManagerCallbacks.php
@@ -13,7 +13,7 @@ class ManagerCallbacks extends BaseController
         $output = array();
 
         foreach ($this->managers as $key => $val) {
-            $output[$key] = isset($input[$key]) ? true : false;
+            $output[$key] = (isset($input[$key]) && $input[$key] == 1) ? true : false;
         }
 
         return $output;
@@ -27,6 +27,8 @@ class ManagerCallbacks extends BaseController
         $classes = $args['class'];
         $option_name = $args['option_name'];
         $checkbox = get_option($option_name);
+
+//        $checked = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
 
         echo '<div class="'.$classes.'"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ($checkbox[$name] ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
     }
