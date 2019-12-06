4fd81e9f5c54c152c62f3d890a2a2b095235a5db
Stefan Oljacic <djedpool@gmail.com>
2019-12-06 22:17:25 +0100

    Adding funcionality for adding a multiple post types.

--- inc/Api/Callbacks/CptCallbacks.php
+++ inc/Api/Callbacks/CptCallbacks.php
@@ -15,25 +15,31 @@ class CptCallbacks
     }
 
     public function cptSanitize($input) {
-        return $input;
+
+        $output = get_option('ultimate_plugin_cpt');
+        foreach ($output as $key => $value) {
+            if ($input['post_type'] === $key) {
+                $output[$key] = $input;
+            } else {
+                $output[$input['post_type']] = $input;
+            }
+        }
+
+        return $output;
     }
 
-    public function textField($args) {
+    public function textField($args){
         $name = $args['label_for'];
         $option_name = $args['option_name'];
-        $input = get_option($option_name);
-        $value =  $input[$name];
 
-
-        echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="' . $value . '" placeholder="'.$args['placeholder'].'">';
+        echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="" placeholder="' . $args['placeholder'] . '">';
     }
 
     public function checkboxField($args){
         $name = $args['label_for'];
         $classes = $args['class'];
         $option_name = $args['option_name'];
-        $checkbox = get_option($option_name);
 
-        echo '<div class="'.$classes.'"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ($checkbox[$name] ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
+        echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class=""><label for="' . $name . '"><div></div></label></div>';
     }
 }
