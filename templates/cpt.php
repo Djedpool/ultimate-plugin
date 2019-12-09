9dd86dc47a98378de6c0e2fdfa640fa21f713af5
Stefan Oljacic <djedpool@gmail.com>
2019-12-09 11:47:30 +0100

    Upadte Custom Post Type functionality.

--- templates/cpt.php
+++ templates/cpt.php
@@ -3,13 +3,17 @@
     <?php settings_errors(); ?>
 
     <ul class="nav nav-tabs">
-        <li class="active"><a href="#tab-1">Your Custom Post Types</a></li>
-        <li><a href="#tab-2">Add Custom Post Type</a></li>
+        <li class="<?php echo !isset($_POST["edit_post"]) ? 'active' : '' ?>"><a href="#tab-1">Your Custom Post Types</a></li>
+        <li class="<?php echo isset($_POST["edit_post"]) ? 'active' : '' ?>">
+            <a href="#tab-2">
+                <?php echo isset($_POST["edit_post"]) ? 'Edit' : 'Add' ?> Custom Post Type
+            </a>
+        </li>
         <li><a href="#tab-3">Export</a></li>
     </ul>
 
     <div class="tab-content">
-        <div id="tab-1" class="tab-pane active">
+        <div id="tab-1" class="tab-pane <?php echo !isset($_POST["edit_post"]) ? 'active' : '' ?>">
 
             <h3>Manage Your Custom Post Types</h3>
 
@@ -36,7 +40,11 @@
                     <td class="text-center"><?php echo $public; ?></td>
                     <td class="text-center"><?php echo $has_archive; ?></td>
                     <td class="text-center">
-                        <a href="#">EDIT</a>
+                        <form method="post" action="" class="inline-block">
+                            <input type="hidden" name="edit_post" value="<?php echo $option['post_type']; ?>">
+                            <?php submit_button('Edit', 'primary small', 'submit', false); ?>
+                        </form>
+
                         <form method="post" action="options.php" class="inline-block">
                             <?php settings_fields( 'ultimate_plugin_cpt_settings' ); ?>
                                 <input type="hidden" name="remove" value="<?php echo $option['post_type']; ?>">
@@ -51,7 +59,7 @@
 
         </div>
 
-        <div id="tab-2" class="tab-pane">
+        <div id="tab-2" class="tab-pane <?php echo isset($_POST["edit_post"]) ? 'active' : '' ?>">
             <form method="post" action="options.php">
                 <?php
                     settings_fields( 'ultimate_plugin_cpt_settings' );
