28d479d1ed64168f15710ad88c8b1c804e34dd31
Stefan Oljacic <djedpool@gmail.com>
2019-12-11 10:54:20 +0100

    Just little fix of spacing on Activate class.

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -12,14 +12,14 @@ class Activate
 
         $default = array();
 
-        if ( ! get_option( 'ultimate_plugin' ) ) {
-            update_option( 'ultimate_plugin', $default );
+        if (!get_option('ultimate_plugin')) {
+            update_option('ultimate_plugin', $default);
         }
-        if ( ! get_option( 'ultimate_plugin_cpt' ) ) {
-            update_option( 'ultimate_plugin_cpt', $default );
+        if (!get_option('ultimate_plugin_cpt')) {
+            update_option('ultimate_plugin_cpt', $default);
         }
-        if ( ! get_option( 'ultimate_plugin_tax' ) ) {
-            update_option( 'ultimate_plugin_tax', $default );
+        if (!get_option('ultimate_plugin_tax')) {
+            update_option('ultimate_plugin_tax', $default);
         }
     }
 }
