ffb6af6ddc754398fdd200f7300e6c51aec817dd
Stefan Oljacic <djedpool@gmail.com>
2019-12-12 23:40:48 +0100

    Setting Up Media Widget and learn thing or two about WP_WIDGET class.

--- inc/Base/WidgetController.php
+++ inc/Base/WidgetController.php
@@ -2,49 +2,17 @@
 /**
  * Created by PhpStorm.
  * User: Stef
- * Date: 12/3/2019
+ * Date: 12/12/2019
  * Time: 7:31 PM
+ * @package UltimatePlugin
  */
 
 namespace Inc\Base;
 
-use Inc\Api\Callbacks\AdminCallbacks;
-use Inc\Api\SettingsApi;
-
 class WidgetController extends BaseController {
 
-    public $settings;
-    public $callback;
-    public $widget_callback;
-    public $subpages = array();
-    public $widgets = array();
-
     public function register() {
-
         if(!$this->activated('media_widget')) return;
-
-        $this->settings = new SettingsApi();
-        $this->callback = new AdminCallbacks();
-
-        $this->setSubPages();
-
-        $this->settings->addSubPages($this->subpages)->register();
     }
 
-    public function activate() {
-        // Probably register widget
-    }
-
-    public function setSubPages() {
-        $this->subpages = array(
-            array(
-                'parent_slug' => 'ultimate_plugin',
-                'page_title'  => 'Widget',
-                'menu_title'  => 'Widget Manager',
-                'capability'  => 'manage_options',
-                'menu_slug'   => 'ultimate_plugin_widget',
-                'callback'    => array($this->callback, 'adminWidget'),
-            )
-        );
-    }
 }
