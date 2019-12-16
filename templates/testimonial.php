ec81cdf0d674a643983a60a63dd1d0c63d311292
Stefan Oljacic <djedpool@gmail.com>
2019-12-16 11:09:59 +0100

    Working on Testimonial shortcodes.

---
+++ templates/testimonial.php
@@ -0,0 +1,7 @@
+<div class="wrap">
+    <h1>Testimonial Shortcode</h1>
+    <p><code>[testimonial-form]</code></p>
+
+    <h1>Testimonial SlideShow Shortcode</h1>
+    <p><code>[testimonial-slideshow]</code></p>
+</div>
