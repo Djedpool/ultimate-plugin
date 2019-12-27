7f689d596a85d7055cd2f20be6285c0cabdf6ad2
Stefan Oljacic <djedpool@gmail.com>
2019-12-27 18:03:20 +0100

    Working od js part of validation and collecting and resetting a default error Messages.

--- src/js/form.js
+++ src/js/form.js
@@ -3,15 +3,45 @@ document.addEventListener('DOMContentLoaded', function (e) {
 
     testimonialForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        console.log('Prevent default');
 
         // reset the form messages
-
-        // validate the email
+        resetMessages();
 
         // collect all the data
+        let data = {
+            name: testimonialForm.querySelector('[name="name"]').value,
+            email: testimonialForm.querySelector('[name="email"]').value,
+            message: testimonialForm.querySelector('[name="message"]').value
+        }
+
+        // validate everything
+        if(!data.name) {
+            testimonialForm.querySelector('[data-error="invalidName"]').classList.add('show');
+            return;
+        }
+
+        if(!validateEmail(data.email)) {
+            testimonialForm.querySelector('[data-error="invalidEmail"]').classList.add('show');
+            return;
+        }
+
+        if(!data.message) {
+            testimonialForm.querySelector('[data-error="invalidMessage"]').classList.add('show');
+            return;
+        }
 
         // ajax http post request
-
+        let url = testimonialForm.dataset.url;
+        console.log(url);
     })
 });
+
+function resetMessages() {
+    document.querySelectorAll('.field-msg').forEach(f => f.classList.remove('show'));
+}
+
+
+function validateEmail(email) {
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
+}
