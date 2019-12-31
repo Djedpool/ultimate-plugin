6d6de82207bf020f11b157ded00654aeaa5b7108
Stefan Oljacic <djedpool@gmail.com>
2019-12-31 13:37:25 +0100

    Finishg Ajax login form and adding support for guteberg for the testimonials.

--- inc/Base/AuthController.php
+++ inc/Base/AuthController.php
@@ -19,6 +19,7 @@ class AuthController extends BaseController
 
         add_action('wp_enqueue_scripts', array($this, 'enqueueScript'));
         add_action('wp_head', array($this, 'addAuthTemplate'));
+        add_action('wp_ajax_nopriv_ultimate_login', array($this, 'login'));
     }
 
     public function addAuthTemplate() {
@@ -36,5 +37,39 @@ class AuthController extends BaseController
 
         wp_enqueue_style('authStyle', $this->plugin_url. 'assets/auth.css');
         wp_enqueue_script('authScript', $this->plugin_url. 'assets/auth.js');
+    }
+
+    public function login() {
+        check_ajax_referer('ajax-login-nonce', 'ultimate_auth');
+
+        $info = array();
+
+        $info['user_login'] = $_POST['username'];
+        $info['user_password'] = $_POST['password'];
+        $info['remember'] = true;
+
+        $user_signon = wp_signon($info, true);
+
+        if(is_wp_error($user_signon)) {
+            echo json_encode(
+                array(
+                    'status' => false,
+                    'message' => 'Wrong username or password.'
+                )
+            );
+
+            die();
+        }
+
+        echo json_encode(
+            array(
+                'status' => true,
+                'message' => 'Login successful, redirecting...'
+            )
+        );
+
+        die();
+
+
     }
 }
--- templates/auth.php
+++ templates/auth.php
@@ -10,12 +10,13 @@
         <label for="password">Password</label>
         <input id="password" type="password" name="password">
         <input class="submit_button" type="submit" value="Login" name="submit">
-        <p class="status"></p>
+        <p class="status" data-message="status"></p>
 
         <p class="actions">
             <a href="<?php echo wp_lostpassword_url(); ?>">Forgot Password?</a> - <a href="<?php echo wp_registration_url(); ?>">Register</a>
         </p>
 
+        <input type="hidden" name="action" value="ultimate_login">
         <?php wp_nonce_field( 'ajax-login-nonce', 'ultimate_auth' ); ?>
     </div>
 </form>
